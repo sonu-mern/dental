28febe35744b1bf4c3bf1b09c47fe8cf9d4c852a
sonu-mern <mahtosonurajkumar108@gmail.com>
2025-12-18 01:26:30 +0530

    Set base path for deployment to '/dental/'

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      base: '/dental/' ,
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
